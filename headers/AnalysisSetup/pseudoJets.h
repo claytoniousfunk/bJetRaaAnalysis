2f383b23182813f040d0e99e29a79efba28d7524
clayton <clayton@debian>
2026-06-16 15:06:18 -0500

    switch to CS PF candidates for FastJet clustering

--- headers/AnalysisSetup/pseudoJets.h
+++ headers/AnalysisSetup/pseudoJets.h
@@ -1,6 +1,6 @@
 // pfCandidateAnalysis variables
 bool doEventMixing = false;
-bool doConstituentSubtraction = false; // true = pfcandAnalyzerCS (CS applied at particle level), false = pfcandAnalyzer + manual rho subtraction
+bool doConstituentSubtraction = true; // true = pfcandAnalyzerCS (CS applied at particle level), false = pfcandAnalyzer + manual rho subtraction
 bool doFastJetClustering = true;      // true = run anti-kT R=0.4 on PF candidates via FastJet (requires -DDO_FASTJET at compile time)
 
 int N_generatedPseudoJets = 100;
