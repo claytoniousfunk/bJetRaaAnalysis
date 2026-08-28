848cf7900afc5ecaf4098df6ef7830d25e76f338
clayton <clayton@debian>
2026-08-27 19:52:07 -0500

    change dataset name

--- src/scanning/PbPb/condor_PbPb_pfCandAnalyzer.py
+++ src/scanning/PbPb/condor_PbPb_pfCandAnalyzer.py
@@ -22,7 +22,7 @@ import os
 # configuration
 # ---------------------------------------------------------------------------
 
-jobname = 'PbPb_pfCandAnalyzer_HISingleMuon_withPFCandidates'
+jobname = 'PbPb_pfCandAnalyzer_HISingleMuon_withPFCandidates_sameEvent'
 
 # input list -- must be the same one PbPb_pfCandAnalyzer.C selects via its
 # doMinBiasSample / doHardProbesSample / doSingleMuonSample flags, because the
