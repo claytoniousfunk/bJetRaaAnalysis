d3f3ced14e1b38d8cbaa25b2c3dbf7e8750b950b
clayton <clayton@debian>
2026-08-28 14:21:27 -0500

    general job directory name

--- src/scanning/PbPb/condor_PbPb_pfCandAnalyzer.py
+++ src/scanning/PbPb/condor_PbPb_pfCandAnalyzer.py
@@ -22,7 +22,7 @@ import os
 # configuration
 # ---------------------------------------------------------------------------
 
-jobname = 'PbPb_pfCandAnalyzer_HISingleMuon_withPFCandidates_sameEvent'
+jobname = 'PbPb_pfCandAnalyzer'
 
 # input list -- must be the same one PbPb_pfCandAnalyzer.C selects via its
 # doMinBiasSample / doHardProbesSample / doSingleMuonSample flags, because the
