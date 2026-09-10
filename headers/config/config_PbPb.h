0533be49a92e474bb6dd997f6a5870108dba71cb
clayton <clayton@debian>
2026-09-10 12:27:10 -0500

    turn off caloJet override

--- headers/config/config_PbPb.h
+++ headers/config/config_PbPb.h
@@ -40,5 +40,5 @@ bool doNoRhoModificationSample = false;
 bool doWithRhoModificationSample = false;
 // reweight functions (for bkg subtraction)
 bool doHiBinReweightToHardProbesJet80 = false;
-bool useCaloJetsOverride = true; // overrides typical PF jet collection with caloJets
+bool useCaloJetsOverride = false; // overrides typical PF jet collection with caloJets
 bool useFlowJetsOverride = false;
