4b75ca273ee5b15ee7deb566d3cd57052677ebda
clayton <clayton@debian>
2026-09-14 21:45:48 -0500

    fix typo

--- src/scanning/pp/pp_scan.C
+++ src/scanning/pp/pp_scan.C
@@ -719,27 +719,27 @@ void pp_scan(TString inputFile, TString outputFile){
 
       // apply trigger if activated in config
       if(applyJet15Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet15_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet15_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet15_v1 == 0) continue;
       }
       if(applyJet30Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet30_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet30_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet30_v1 == 0) continue;
       }
       if(applyJet40Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet40_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet40_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet40_v1 == 0) continue;
       }
       if(applyJet60Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet60_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet60_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet60_v1 == 0) continue;
       }
       if(applyJet80Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet80_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet80_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet80_v1 == 0) continue;
       }
       if(applyJet100Trigger){
-	if(useCaloJetOverride) if(em->HLT_HIAK4CaloJet100_v1 == 0) continue;
+	if(useCaloJetsOverride) if(em->HLT_HIAK4CaloJet100_v1 == 0) continue;
 	  else if(em->HLT_HIAK4PFJet100_v1 == 0) continue;
       }
       if(applyAntiMu5Jet30Trigger){
