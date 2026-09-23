c95c3b5b2ebc3ba5ee4c1bd328301dc32f803535
clayton <clayton@debian>
2026-09-23 11:42:13 -0500

    run pp HighEGJet Jet80 w caloJets

--- headers/config/config_pp.h
+++ headers/config/config_pp.h
@@ -17,7 +17,7 @@ bool applyJet15Trigger = false;
 bool applyJet30Trigger = false;
 bool applyJet40Trigger = false;
 bool applyJet60Trigger = false;
-bool applyJet80Trigger = false;
+bool applyJet80Trigger = true;
 bool applyJet100Trigger = false;
 bool applyAntiMu5Jet30Trigger = false;
 bool applyAntiMu5Jet40Trigger = false;
@@ -36,8 +36,8 @@ bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // select dataset
 bool doSingleMuonSample = false;
-bool doMinBiasSample = true;
-bool doHighEGJetSample = false;
+bool doMinBiasSample = false;
+bool doHighEGJetSample = true;
 // jet mods
 bool useCaloJetsOverride = true;
 bool useManualJEC = true;
