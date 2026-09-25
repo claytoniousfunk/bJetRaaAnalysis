ff691714e42f80ccc7ef7b5c93ff4453d9a961e3
clayton <clayton@debian>
2026-09-25 17:15:10 -0500

    scan SingleMuon

--- headers/config/config_pp.h
+++ headers/config/config_pp.h
@@ -18,7 +18,7 @@ bool applyJet30Trigger = false;
 bool applyJet40Trigger = false;
 bool applyJet60Trigger = false;
 bool applyJet80Trigger = false;
-bool applyJet100Trigger = true;
+bool applyJet100Trigger = false;
 bool applyAntiMu5Jet30Trigger = false;
 bool applyAntiMu5Jet40Trigger = false;
 bool applyAntiMu5Jet60Trigger = false;
@@ -35,7 +35,7 @@ bool apply_JER_smear = false;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // select dataset
-bool doSingleMuonSample = false;
+bool doSingleMuonSample = true;
 bool doMinBiasSample = false;
 bool doHighEGJetSample = true;
 // jet mods
